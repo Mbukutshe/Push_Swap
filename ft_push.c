08e8c5c2d14700de65de6b7adc92bc610a810777
Mbukutshe <kmbukuts@student.wethinkcode.co.za>
2019-07-16 10:40:23 +0200

    Add files via upload

--- ft_push.c
+++ ft_push.c
@@ -6,7 +6,7 @@
 /*   By: kmbukuts <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/03 14:31:56 by kmbukuts          #+#    #+#             */
-/*   Updated: 2019/07/16 09:11:32 by kmbukuts         ###   ########.fr       */
+/*   Updated: 2019/07/16 10:40:03 by kmbukuts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,18 @@ void		ft_push_a(stack_a **a, stack_b **b, int *s)
 	stack_a **pb;
 	int		size;
 
-	pb = b;
-	pa = malloc(sizeof(stack_a));
-	pa->v = (*pb)->v;
-	pa->p = (*pb)->p;
-	pa->next = *a;
-	*a = pa;
-	ft_remove_first(&*b);
-	size = stack_b_size("minus");
-	stack_a_size(s, "add");
+	if (stack_b_size("") > 0)
+	{
+		pb = b;
+		pa = malloc(sizeof(stack_a));
+		pa->v = (*pb)->v;
+		pa->p = (*pb)->p;
+		pa->next = *a;
+		*a = pa;
+		ft_remove_first(&*b);
+		size = stack_b_size("minus");
+		stack_a_size(s, "add");
+	}
 	return ;
 }
 
@@ -36,15 +39,18 @@ void		ft_push_b(stack_a **a, stack_b **b, int *s)
 	stack_b *pb;
 	int		size;
 
-	pa = a;
-	pb = malloc(sizeof(stack_b));
-	pb->v = (*pa)->v;
-	pb->p = (*pa)->p;
-	pb->next = *b;
-	*b = pb;
-	ft_remove_first(&*a);
-	stack_a_size(s, "minus");
-	size = stack_b_size("add");
+	if (*s > 0)
+	{
+		pa = a;
+		pb = malloc(sizeof(stack_b));
+		pb->v = (*pa)->v;
+		pb->p = (*pa)->p;
+		pb->next = *b;
+		*b = pb;
+		ft_remove_first(&*a);
+		stack_a_size(s, "minus");
+		size = stack_b_size("add");
+	}
 	return ;
 }
 
